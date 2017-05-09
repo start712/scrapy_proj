f0c936749c8022952c926b411b6e4ca23787842a
start712 <start712@ProtonMail.com>
2017-05-09 11:28:32 +0800

    update piplines

--- announcements_monitor/announcements_monitor/pipelines.py
+++ announcements_monitor/announcements_monitor/pipelines.py
@@ -34,7 +34,7 @@ import logging.handlers
 LOG_FILE = r'%s/log/duplicate_entry(%s).log' %(os.getcwd(),datetime.datetime.date(datetime.datetime.today()))
 
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024 * 1024, backupCount=5)  # 实例化handler
-fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+fmt = '%(asctime)s: %(message)s'
 
 formatter = logging.Formatter(fmt)  # 实例化formatter
 handler.setFormatter(formatter)  # 为handler添加formatter
