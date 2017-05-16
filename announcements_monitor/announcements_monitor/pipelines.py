f98b90ed06220754eac15e1b23f32edd4311f162
start712 <start712@ProtonMail.com>
2017-05-16 15:14:39 +0800

    更新了数据库里土地状态的字段fixture_date

--- announcements_monitor/announcements_monitor/pipelines.py
+++ announcements_monitor/announcements_monitor/pipelines.py
@@ -97,6 +97,7 @@ class AnnouncementsMonitorPipeline(object):
             item["monitor_key"] = "raw_page/%s/%s/%s/%s" % (item["monitor_id"], item["monitor_date"], re_type, item["monitor_title"])#re.sub(r'\s+', '', "raw_page/%s/%s/%s" % (item["monitor_id"], item["monitor_date"], item["monitor_title"]))
 
         item['content_detail']['status'] = item['parcel_status']
+        item['content_detail']['fixture_date'] = item["monitor_date"]
         item['content_detail'] = json.dumps(item['content_detail'])#json.dumps({key: re.sub(r'\s+', '', str(item['content_detail'][key])) for key in item['content_detail']})
 
         query=self.dbpool.runInteraction(self._conditional_insert,item)#调用插入的方法
