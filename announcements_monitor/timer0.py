2b7f5229a7bdfd12fd68144b901b670b0cf39143
start712 <start712@ProtonMail.com>
2017-07-20 11:39:52 +0800

    增加定时器timer0

---
+++ announcements_monitor/timer0.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-  
+"""
+--------------------------------
+    @Author: Dyson
+    @Contact: Weaver1990@163.com
+    @file: mytimer.py
+    @time: 2017/7/20 11:18
+--------------------------------
+"""
+import sys
+import os
+
+sys.path.append(sys.prefix + "\\Lib\\MyWheels")
+reload(sys)
+sys.setdefaultencoding('utf8')
+import mytimer
+mytimer = mytimer.mytimer()
+
+class timer0(object):
+    def __init__(self):
+        pass
+    def main(self):
+        mytimer.cmd_timer('python %s' %(os.getcwd() + r'\controller.py'), 600)
+
+if __name__ == '__main__':
+    timer0 = timer0()
+    timer0.main()
